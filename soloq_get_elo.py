7e13e33c4b248aedea90149ef8cffb699ee322c4
PlasmOwO <thomas.plasmo@gmail.com>
2025-01-25 19:35:34 +0100

    Add function for soloq tracking

---
+++ soloq_get_elo.py
@@ -0,0 +1,93 @@
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+import sqlite3
+import datetime
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+tiers = {
+    'IRON': 0,
+    'BRONZE': 400,
+    'SILVER': 800,
+    'GOLD': 1200,
+    'PLATINUM': 1600,
+    'EMERALD' : 2000,
+    'DIAMOND': 2400,
+    'MASTER': 2800
+}
+
+divisions = {
+    'IV': 0,
+    'III': 100,
+    'II': 200,
+    'I': 300
+}
+def adapt_datetime(val):
+    """Convert datetime.datetime object to ISO 8601 string."""
+    return val.isoformat()
+
+def convert_datetime(val):
+    """Convert ISO 8601 string to datetime.datetime object."""
+    return datetime.datetime.fromisoformat(val.decode())
+
+# Register the adapter and converter with sqlite3
+sqlite3.register_adapter(datetime.datetime, adapt_datetime)
+sqlite3.register_converter("datetime", convert_datetime)
+
+def lol_rank_to_numeric(player_tier : str ,player_lp : int ,player_division : str = '' ) -> int : 
+    """Transform the rank of a player into a numeric value
+
+    Args:
+        player_tier (str): Tier of the player. For example : "GOLD"
+        player_lp (int): LP of the player.
+        player_division (str, optional): Division of the player, for example : "II". Defaults to ''. No Division for MASTER+
+
+    Returns:
+        int: The numerical value of the rank of a player.
+    """
+    print(player_lp)
+    if player_tier =='MASTER' or player_tier == 'GRANDMASTER' or player_tier == 'CHALLENGER':
+        return 2800 + player_lp
+    if player_division is None or player_tier is None or player_tier is None :
+        return None
+    return tiers[player_tier] + divisions[player_division] + player_lp
+
+player_puuid = os.environ['LIST_PLAYER_PUUID'].split(",")
+
+list_tracking_player = []
+keys_dict = ["tier","rank","leaguePoints"]
+for puuid in player_puuid :
+    try :
+        id_request = requests.get(f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={API_KEY}")
+        id = id_request.json()['id']
+    except Exception as  e:
+        print("Erreur dans la récupération de l'id", id_request)
+        exit()
+
+    try :
+        tracking_player_request = requests.get(f"https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{id}?api_key={API_KEY}")
+        tracking_player_json = tracking_player_request.json()
+    except Exception as e:
+        print("Erreur dans la récupération des informations du joueur", tracking_player_request)
+        exit()
+    
+    rank_player = []
+    for key in keys_dict:
+        if len(tracking_player_json) ==0 :
+            rank_player.append(None)
+        else :
+            rank_player.append(tracking_player_json[0].get(key))
+
+    #FIXME when Master will be reached
+    list_tracking_player.append(lol_rank_to_numeric(rank_player[0],rank_player[2],rank_player[1]))
+print(list_tracking_player)
+
+con = sqlite3.connect('soloq_tracking.db')
+cursor = con.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS soloq_tracking (date TIMESTAMP PRIMARY KEY, TOP_RANK TEXT, JNG_RANK TEXT, MID_RANK TEXT, ADC_RANK TEXT, SUP_RANK TEXT)")
+date_now = datetime.datetime.now()
+cursor.execute("INSERT INTO soloq_tracking (date, TOP_RANK, JNG_RANK, MID_RANK, ADC_RANK, SUP_RANK) VALUES (?,?,?,?,?,?)",(date_now,list_tracking_player[0],list_tracking_player[1],list_tracking_player[2],list_tracking_player[3],list_tracking_player[4]))
+con.commit()
+con.close()
---
+++ soloq_tracking.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import sqlite3
+import datetime
+import plotly.express as plty
+import plotly.graph_objects as go
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import requests
+from typing import Literal
+#https://github.com/Allan-Cao
+
+API_KEY = os.getenv("API_KEY")
+
+
+def get_league_HL_player() -> tuple:
+    """Request the API to get all master+ players
+
+    Returns:
+        tuple: A list with challenger and GM players and another with master players.
+    """
+
+    challenger_call = requests.get(f"https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={API_KEY}")
+    challenger_call.raise_for_status()
+    grandmaster_call = requests.get(f"https://euw1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key={API_KEY}")
+    grandmaster_call.raise_for_status()
+    master_call = requests.get(f"https://euw1.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key={API_KEY}")
+    master_call.raise_for_status()
+    
+    return challenger_call.json()['entries'] + grandmaster_call.json()['entries'] , master_call.json()['entries']
+
+
+def calculate_cutoffs(gm_chall_player : list,master_player : list) -> tuple :
+    """Calculate how many lp is needed to be challenger or grandmaster
+
+    Args:
+        gm_chall_player (list): List of challenger and grandmaster players
+        master_player (list): List of master players
+
+    Returns:
+        tuple: Tuple containing the cutoffs (lp needed) for challenger and grandmaster
+    """
+    
+    if len(gm_chall_player) < 1000:
+        return 500,200
+    hl_player = gm_chall_player + master_player
+    hl_player.sort(key=lambda x: x["leaguePoints"], reverse=True)
+    chall_player_lbBased = hl_player[0:300]
+    gm_player_lbBased = hl_player[300:700+300]
+
+    challenger_cutoff = chall_player_lbBased[-1]["leaguePoints"] + 1
+    grandmaster_cutoff = gm_player_lbBased[-1]["leaguePoints"] + 1
+    return challenger_cutoff, grandmaster_cutoff
+
+#HL Cutoffs
+gm_chall_player,master_player = get_league_HL_player()
+challenger_cutoff, grandmaster_cutoff = calculate_cutoffs(gm_chall_player,master_player)
+print(challenger_cutoff, grandmaster_cutoff)
+
+tiers = {
+    'IRON': 0,
+    'BRONZE': 400,
+    'SILVER': 800,
+    'GOLD': 1200,
+    'PLATINUM': 1600,
+    'EMERALD' : 2000,
+    'DIAMOND': 2400,
+    'MASTER': 2800,
+    'GRANDMASTER': 2800+grandmaster_cutoff,
+    'CHALLENGER': 2800+challenger_cutoff
+}
+
+divisions = {
+    'IV': 0,
+    'III': 100,
+    'II': 200,
+    'I': 300
+}
+
+#DB connection
+con = sqlite3.connect('soloq_tracking.db')
+cursor = con.cursor()
+database_table = cursor.execute("SELECT * FROM soloq_tracking").fetchall()
+soloq_df = pd.DataFrame(database_table, columns=["date","TOP_RANK","JNG_RANK","MID_RANK","ADC_RANK","SUP_RANK"])
+soloq_df["date"] = pd.to_datetime(soloq_df["date"])
+
+
+#Plot
+images_path = "images/rank_emblems/"
+fig = go.Figure()
+
+for role in ["TOP_RANK","JNG_RANK","MID_RANK","ADC_RANK","SUP_RANK"] :
+    fig.add_trace(go.Scatter(x=soloq_df["date"], y=soloq_df[role],mode='lines+markers',name=role))
+
+
+fig.update_layout(
+    xaxis_title='Date',
+    yaxis_title='Rank',
+    legend_title_text='Roles',
+    yaxis=dict(
+        tickmode='array',
+        tickvals=list(tiers.values()),
+        ticktext=list(tiers.keys()),
+        showgrid=True,
+        gridwidth=1,
+        zeroline=False,
+        minor=dict(
+            tickmode='linear',
+            tick0=0,
+            dtick=100,
+            gridcolor='LightGray',
+            gridwidth=0.5
+        )
+    )
+)
+
+fig.show()
+con.close()
